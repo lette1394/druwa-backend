5cd870879b988ae5a1c110384dbfd48da11264c6
Jaeeun Na <lette1394@gmail.com>
2020-01-12 04:30:56 +0900

    Composite-id class does not override hashCode()

--- src/main/java/me/druwa/be/domain/drama/model/DramaLike.java
+++ src/main/java/me/druwa/be/domain/drama/model/DramaLike.java
@@ -2,18 +2,17 @@ package me.druwa.be.domain.drama.model;
 
 import java.io.Serializable;
 import javax.persistence.Column;
-import javax.persistence.ConstraintMode;
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import me.druwa.be.domain.common.model.Timestamp;
 import me.druwa.be.domain.user.model.User;
@@ -22,6 +21,7 @@ import me.druwa.be.domain.user.model.User;
 @Table(name = "drama_like_")
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "dramaLikeId")
 public class DramaLike {
     @EmbeddedId
     private DramaLikeKey dramaLikeId;
@@ -40,6 +40,7 @@ public class DramaLike {
     private Timestamp timestamp;
 
     @Embeddable
+    @EqualsAndHashCode
     public static class DramaLikeKey implements Serializable {
         @Column(name = "drama_id")
         private Long dramaId;
